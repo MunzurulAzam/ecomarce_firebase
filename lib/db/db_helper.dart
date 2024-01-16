5ef5152a26f0bc03c16ecc10779d8a5507b594c9
MacBook Air <macbookair@MacBooks-Air.lan>
2024-01-16 14:19:48 +0600

    ..

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommercefirebase/models/category_model.dart';
 
 class DbHelper {
   static final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -8,5 +9,11 @@ class DbHelper {
   static Future<bool> isAdmin(String uid) async {
     final snapshot = await _db.collection(collectionAdmin).doc(uid).get();
     return snapshot.exists;
+  }
+  static Future<void> addCategory(CategoryModel categoryModel){
+    final catDoc = _db.collection(collectionCategory).doc();//...............create document for firebase
+
+    categoryModel.categoryId = catDoc.id;
+    return catDoc.set(categoryModel.toMap());
   }
 }
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,14 +1,26 @@
+import 'package:ecommercefirebase/utils/widget_functions.dart';
 import 'package:flutter/material.dart';
+
 class CategoryPage extends StatelessWidget {
   static const String routeName = '/category';
+
   const CategoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('CATEGORY LIST'),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          showSingleTextInputDialog(
+            context: context,
+            title: 'Add New Title',
+            onSubmit: (value){
+              print(value);
+            },
+          );
         },
         child: const Icon(Icons.add),
       ),
